bb83dcac4c13719fc4af9d7f708dfdce4f591459
Anthony <ajsawyer94@gmail.com>
2026-03-04 18:01:41 -0800

    Add schedules models to admin: Sport, Team, Game

--- schedules/admin.py
+++ schedules/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Game, Sport, Team
+
+
+@admin.register(Sport)
+class SportAdmin(admin.ModelAdmin):
+    list_display = ('name', 'league', 'slug')
+    search_fields = ('name', 'league')
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'abbreviation', 'sport', 'espn_id')
+    list_filter = ('sport',)
+    search_fields = ('name', 'abbreviation')
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('home_team', 'away_team', 'sport', 'start_time', 'streaming_service', 'status')
+    list_filter = ('sport', 'streaming_service', 'status')
+    search_fields = ('home_team__name', 'away_team__name')
+    readonly_fields = ('espn_id',)
